b255a33f38497bf5d1494256575c69bba17bd720
seoyeonko <seoyeon99o@naver.com>
2023-06-14 21:58:20 +0900

    faet: GET /api/articles/{id}, DELETE /api/articles/{id}

--- src/main/java/sean/blog/blogapplication/controller/BlogApiController.java
+++ src/main/java/sean/blog/blogapplication/controller/BlogApiController.java
@@ -3,10 +3,7 @@ package sean.blog.blogapplication.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sean.blog.blogapplication.domain.Article;
 import sean.blog.blogapplication.dto.AddArticleRequest;
 import sean.blog.blogapplication.dto.ArticleResponse;
@@ -35,5 +32,20 @@ public class BlogApiController {
         List<ArticleResponse> articles = blogService.findAll().stream().map(ArticleResponse::new).toList();
 
         return ResponseEntity.ok().body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/sean/blog/blogapplication/service/BlogService.java
+++ src/main/java/sean/blog/blogapplication/service/BlogService.java
@@ -23,4 +23,14 @@ public class BlogService {
     public List<Article> findAll() {
         return blogRepository.findAll();
     }
+
+    // 블로그 글 하나 조회 메서드
+    public Article findById(long id) {
+        return blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(("not found: "+ id)));
+    }
+
+    // 블로그 글 삭제 메서드
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
 }
