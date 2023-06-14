29780cfe08d9a7fc03d09cbe5a8cca05f109f527
seoyeonko <seoyeon99o@naver.com>
2023-06-14 21:36:48 +0900

    feat: GET /api/articles

--- src/main/java/sean/blog/blogapplication/service/BlogService.java
+++ src/main/java/sean/blog/blogapplication/service/BlogService.java
@@ -6,6 +6,8 @@ import sean.blog.blogapplication.domain.Article;
 import sean.blog.blogapplication.dto.AddArticleRequest;
 import sean.blog.blogapplication.repository.BlogRepository;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final 이 붙거나 @NotNull 이 붙은 필드의 생성자 추가
 @Service // 빈으로 등록
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
     // save(): JpaRepository 에서 지원하는 저장 메서드 AddArticleRequest 클래스에 저장되는 값들을 article 데이터베이스에 저장
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 모든 글 조회 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
