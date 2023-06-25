be65b94d97925ce0b7f70380d19ac547584e85dc
seoyeonko <seoyeon99o@naver.com>
2023-06-26 00:50:53 +0900

    study: add logout method

--- src/main/java/sean/blog/blogapplication/controller/UserApiController.java
+++ src/main/java/sean/blog/blogapplication/controller/UserApiController.java
@@ -1,7 +1,12 @@
 package sean.blog.blogapplication.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import sean.blog.blogapplication.dto.AddUserRequest;
 import sean.blog.blogapplication.service.UserService;
@@ -15,5 +20,12 @@ public class UserApiController {
     public String signup(AddUserRequest request) {
         userService.save(request); // 회원가입 메서드 호출
         return "redirect:/login"; // 회원가입 완료된 이후에 로그인 페이지 리다이렉트
+    }
+
+    // GET /logout 요청을 보내면, 로그아웃 담당하는 핸들러 SecurityContextLogoutHandler의 logout() 메서드 호출하여 로그아웃
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
